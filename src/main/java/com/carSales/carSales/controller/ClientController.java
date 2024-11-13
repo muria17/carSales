e16c582b4e3c1403759e061f951078779a465c74
Muria <financas.antonio@gmail.com>
2024-11-13 09:06:03 -0300

    criando delete e saved de customers

--- src/main/java/com/carSales/carSales/controller/ClientController.java
+++ src/main/java/com/carSales/carSales/controller/ClientController.java
@@ -4,10 +4,12 @@ import com.carSales.carSales.domain.Client;
 import com.carSales.carSales.repository.ClientRepository;
 import com.carSales.carSales.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("client")
@@ -21,12 +23,25 @@ public class ClientController {
 
     @PostMapping
     @Transactional
-    public Client create(@RequestBody Client register) {
-        return repository.save(register);
+    public ResponseEntity<Client> create(@RequestBody Client register) {
+        Client saved = repository.save(register);
+        return ResponseEntity.ok(saved);
     }
 
     @GetMapping
     public List<Client> findClients(){
         return service.findClientById();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<Client> client = repository.findById(id);
+        if (client.isPresent()) {
+            repository.delete(client.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
