402d2ce61b43403004ddfc693265732b9a64827d
Muria <financas.antonio@gmail.com>
2024-11-21 08:34:35 -0300

    possibilitando criação de novos usuarios

--- src/main/java/com/carSales/carSales/controller/CarStockController.java
+++ src/main/java/com/carSales/carSales/controller/CarStockController.java
@@ -4,10 +4,12 @@ import com.carSales.carSales.domain.CarStock;
 import com.carSales.carSales.repository.CarStockRepository;
 import com.carSales.carSales.service.CarStockService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("carStock")
@@ -21,12 +23,24 @@ public class CarStockController {
 
     @PostMapping
     @Transactional
-    public CarStock create(@RequestBody CarStock register) {
-        return repository.save(register);
+    public ResponseEntity<CarStock> create(@RequestBody CarStock register) {
+        CarStock saved = repository.save(register);
+        return ResponseEntity.ok(saved);
     }
 
     @GetMapping
     public List<CarStock> findClients(){
         return service.findClientById();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<CarStock> carStock = repository.findById(id);
+        if (carStock.isPresent()) {
+            repository.delete(carStock.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
